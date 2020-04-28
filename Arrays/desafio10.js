c98896c8fdf227c824b9a6dfd3afa72436328783
eneas-hsn <eneashsn@gmail.com>
2020-04-28 15:25:40 -0300

    Update desafio10.js

--- Arrays/desafio10.js
+++ Arrays/desafio10.js
@@ -1,1 +1,15 @@
 var valores=[]
+while (true) {
+    var valor= prompt('Digite um npumero ou S para sair')
+    if (valor=='S') {
+        break
+    } else{
+        valores.push(Number(valor))
+    }
+}
+var soma=0
+for(var i=0;i<valores.length;i++){
+    soma+=valores[i]
+}
+var resultado=soma/valores.length
+console.log(resultado);
