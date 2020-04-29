f0373be99f3d8587f3d27d3beb15b18bc4af038b
eneas-hsn <eneashsn@gmail.com>
2020-04-29 15:59:25 -0300

    Create hash.js

---
+++ Arrays/hash.js
@@ -0,0 +1,11 @@
+var convidados={}
+convidados['abc']={
+    nome:'Neneas', idade:16, id:1
+
+}
+convidados['def']={
+    nome:'enesio',
+    idade:18,
+    id:2
+}
+console.log(convidados['abc'].id)
