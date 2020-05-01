82596d58d89faa8644f43b43fbb0834b39c81f8f
eneas-hsn <eneashsn@gmail.com>
2020-04-30 21:06:31 -0300

    Update filter.js

--- Arrays/filter.js
+++ Arrays/filter.js
@@ -1,6 +1,45 @@
-var numeros=[10,18,1,15]
+/*var numeros=[10,18,1,15]
 var maiorQue10=function (item){
     return item>10
 }
 var novosNumeros=numeros.filter(maiorQue10)
 console.log(novosNumeros)
+
+var nome=['eneas', 'zeneas','geneas']
+var filtrarZ=function (item) {
+    if (item.charAt(0)=='z'){
+        return true
+    }else{
+        return false
+    }
+}
+var convidados= nome.filter(filtrarZ)
+console.log(convidados)
+
+var numeros=[1,2,3,4,5,6,7,8,9,10]
+var pares=function (x) {
+    return x%2==0
+}
+var impares=function (x) {
+    return x%2!=0
+}
+var numerosPares= numeros.filter(pares)
+var numerosImpares=numeros.filter(impares)
+console.log(numerosImpares)
+var filmes=[
+    titanic={titulo:'filme1', duracao:100, nota:10},
+    aranha={titulo:'filme2', duracao:150, nota:7},
+    avengers={titulo:'filme3', duracao:200, nota:5}
+]
+var corte=8
+var melhores= function (item) {
+    return item.nota>corte
+}
+var piores = function(item){
+    return item.nota<corte
+}
+var melhoresFilmes=filmes.filter(melhores)
+var pioresFilmes=filmes.filter(piores)
+console.log(melhoresFilmes)
+console.log(pioresFilmes)
+*/
