adc677be9ccb7806ecf4901c9cead67400339afe
eneas-hsn <eneashsn@gmail.com>
2020-05-02 00:49:22 -0300

    Update juntandotudo.js

--- Arrays/juntandotudo.js
+++ Arrays/juntandotudo.js
@@ -1,4 +1,4 @@
-var numeros= [10,18,1,15,2,12,21,33,100]
+/*var numeros= [10,18,1,15,2,12,21,33,100]
 var pares= function (item) {
     if (item%2==0) {
         return item
@@ -8,4 +8,19 @@ var dobrados= function (item) {
     return item*2
 }
 var nParesDobrados= numeros.filter(pares).map(dobrados)
-console.log(nParesDobrados)
+console.log(nParesDobrados)*/
+var carros = [{marca: 'Audi', cor: 'preto'},
+ {marca: 'Audi', cor: 'branco'},
+ {marca: 'Ferarri', cor: 'vermelho'},
+ {marca: 'Ford', cor: 'branco'},
+ {marca: 'Peugot', cor: 'branco'}];
+ function groupBy(array,prop) {
+     var value= array.reduce(function (total,item) {
+         var key=item[prop]//item marca
+         total[key]= (total[key] || []).concat(item)
+         return total
+     }, {})
+     return value
+ }
+  var agrupados=groupBy(carros,'marca')
+  console.log(agrupados)
