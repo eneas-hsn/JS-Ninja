c79d6ef4b722d0850d9b644859b8d32e26ccfdfe
eneas-hsn <eneashsn@gmail.com>
2020-05-02 01:55:23 -0300

    Update juntandotudo.js

--- Arrays/juntandotudo.js
+++ Arrays/juntandotudo.js
@@ -8,11 +8,12 @@ var dobrados= function (item) {
     return item*2
 }
 var nParesDobrados= numeros.filter(pares).map(dobrados)
-console.log(nParesDobrados)*/
+console.log(nParesDobrados)
+...
 var carros = [{marca: 'Audi', cor: 'preto'},
  {marca: 'Audi', cor: 'branco'},
- {marca: 'Ferarri', cor: 'vermelho'},
  {marca: 'Ford', cor: 'branco'},
+ {marca: 'Ferarri', cor: 'vermelho'},
  {marca: 'Peugot', cor: 'branco'}];
  function groupBy(array,prop) {
      var value= array.reduce(function (total,item) {
@@ -23,4 +24,26 @@ var carros = [{marca: 'Audi', cor: 'preto'},
      return value
  }
   var agrupados=groupBy(carros,'marca')
-  console.log(agrupados)
+  console.log(agrupados)*/
+  Array.prototype.groupBy = function(prop) {
+    var value = this.reduce(function(total, item) {
+    var key = item[prop];
+    total[key] = (total[key] || []).concat(item);
+    return total;
+    }, {});
+    return value;
+   }
+   var convidados = [
+    {nome: 'Felipe', idade: 37, genero: 'masculino'},
+    {nome: 'Amanda', idade: 17, genero: 'feminino'},
+    {nome: 'João', idade: 27, genero: 'masculino'},
+    {nome: 'Daniel', idade: 21, genero: 'masculino'},
+    {nome: 'Helena', idade: 21, genero: 'feminino'}
+   ];
+   var convidadosFiltrados = convidados.sort(function(a, b) {
+    return (a.nome > b.nome) ? 1 : -1; // 1 B precede A, -1 A precede B
+   }).filter(function(item) {
+    return item.idade >= 18;
+   }).groupBy('genero');
+   console.log(convidadosFiltrados);
+   
